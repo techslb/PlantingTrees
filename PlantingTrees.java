1bcf7a40d85b0d22f5d29463b1204c33b4ce1c68
techslb <techslb@outlook.com>
2025-11-24 14:56:01 -0600

    Create PlantingTrees.java

---
+++ PlantingTrees.java
@@ -0,0 +1,44 @@
+// Define your tree class in this file!
+public class Tree {
+  public static void main(String[] args) {
+    /* Task performed: Planting an ASCII art tree and introducing myself
+    */
+    // This block is the introduction mentioned in the method block comment
+    System.out.println("Greetings, my name is techslb, I am learning how to code in Java.");
+    System.out.println("I'm going to plant a tree today. Let's get ready to rumble!\n");
+
+    // Creating the ASCII tree art
+    System.out.println("  *  ");
+    System.out.println(" *** ");
+    System.out.println(" *** ");
+    System.out.println("  *  ");
+    System.out.println("  *  ");
+    
+    // Creating a second tree type
+    System.out.println("");
+    System.out.println("   *   ");
+    System.out.println("  * *  ");
+    System.out.println(" * * * ");
+    System.out.println("* * * *");
+    System.out.println("* * * *");
+    System.out.println("* * * *");
+    System.out.println("   *   ");
+    System.out.println("   *   ");
+    System.out.println("   *   ");
+    System.out.println("   *   ");
+    System.out.println("   *   ");
+    System.out.println("~~~*~~~");
+    
+    // Creating a third tree type
+    System.out.println("");
+    System.out.println("   M   ");
+    System.out.println("  MMM  ");
+    System.out.println(" MMMMM ");
+    System.out.println("|mm|mm|");
+    System.out.println("|ww|ww|");
+    System.out.println("WWWWWWW");
+    System.out.println("   |   ");
+    System.out.println("___|___"); 
+
+  }
+}
